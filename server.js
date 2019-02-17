fd221dba1fa9d07a4d9cff56e08e912759b16c88
Michel Pinzetta Gayeski <michel@reclameaqui.com.br>
2019-02-17 11:21:11 -0300

    set configs application

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+    // logRequests: true,
+    //--disable-dev-shm-usage
+    //--user-data-dir=/tmp
+    //--disk-cache-dir=/tmp
+    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--disable-dev-shm-usage', '--hide-scrollbars', '--disk-cache-dir=/tmp']
+});
 
 server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.blockResources());
+server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+server.use(prerender.removeMetaTagFragment());
+server.use(prerender.s3HtmlCache());
 
 server.start();
