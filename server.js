45b017e9449117f31c981e824bfd9747224788a3
Michel Pinzetta <michel@reclameaqui.com.br>
2019-10-11 16:12:18 -0300

    add user-agent prerender & no removeScriptTags & use stable version chrome

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ var server = prerender({
     //--disable-dev-shm-usage
     //--user-data-dir=/tmp
     //--disk-cache-dir=/tmp
-    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--disable-dev-shm-usage', '--hide-scrollbars', '--disk-cache-dir=/tmp']
+    userAgent: ' Prerender (+https://github.com/prerender/prerender)',
+    chromeFlags: ['--user-agent=" Prerender (+https://github.com/prerender/prerender)"', '--disable-web-security','--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--disable-dev-shm-usage', '--hide-scrollbars', '--disk-cache-dir=/tmp']
 });
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.blockResources());
-server.use(prerender.removeScriptTags());
+// server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 server.use(prerender.removeMetaTagFragment());
 server.use(prerender.s3HtmlCache());
